b85b67017f32561da6aa08f88eea807f7b2aa099
jk <jk@spasitel.com>
2023-05-23 16:56:22 +0200

    update + tests

--- backup.py
+++ backup.py
@@ -1,8 +1,10 @@
+""" Encrypt files and directories for backup on untrusted storage"""
 import os
 
 import base64
-from pathlib import Path
 import logging
+
+# pylint: disable=logging-fstring-interpolation
 
 # https://cryptography.io/en/latest/hazmat/primitives/key-derivation-functions/#scrypt
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
@@ -19,14 +21,50 @@ SALT_SIZE_BYTES = 16
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
-_KEYSTORE={} # cached keys for detected salts
+_KEYSTORE = {}  # cached keys for detected salts
+
+
+class EncryptionKey:
+    """Encryption key with all data needed to derive it back from password"""
+
+    def __init__(self, password: str, salt: bytes | None = None):
+        """
+        Generate new key wit optional salt
+        If salt is not provided, it will be generated. It must be provided
+        for successful decryption with previously generated key.
+        """
+        if salt is None:
+            salt = os.urandom(SALT_SIZE_BYTES)
+        kdf = Scrypt(
+            salt=salt,
+            length=32,
+            n=SCRYPT_N,
+            r=8,
+            p=1,
+        )
+        self.key = kdf.derive(password.encode("utf-8"))
+        self.salt = salt
+        # verify
+        kdf = Scrypt(
+            salt=salt,
+            length=32,
+            n=SCRYPT_N,
+            r=8,
+            p=1,
+        )
+        kdf.verify(password.encode("utf-8"), self.key)
+
+
 class FileEncryptor:
-    #TODO: save salt in the file
-    def __init__(self, path, key):
+    """File wrapper that encrypts the content of the file on the fly"""
+
+    # TODO: save salt in the file
+    # pylint: disable=invalid-name
+    def __init__(self, path: str, key: EncryptionKey):
         self.key = key
         self.iv = os.urandom(IV_SIZE_BYTES)
         self.encryptor = Cipher(
-            algorithms.AES256(key),
+            algorithms.AES256(self.key.key),
             modes.GCM(self.iv),
         ).encryptor()
         self.path = path
@@ -34,6 +72,7 @@ class FileEncryptor:
         self.finalized = False
 
     def read(self, size=None):
+        """reads and encrypts the file content, returns encrypted data"""
         if self.finalized:
             return b""
         args = []
@@ -47,20 +86,30 @@ class FileEncryptor:
             encrypted_data = self.encryptor.update(unencrypted_data)
             if size is None:
                 encrypted_data += (
-                    self.encryptor.finalize() + self.encryptor.tag + self.iv
+                    self.encryptor.finalize()
+                    + self.encryptor.tag
+                    + self.iv
+                    + self.key.salt
                 )
                 self.finalized = True
         else:
-            encrypted_data = self.encryptor.finalize() + self.encryptor.tag + self.iv
+            encrypted_data = (
+                self.encryptor.finalize() + self.encryptor.tag + self.iv + self.key.salt
+            )
             self.finalized = True
         return encrypted_data
 
-    def close():
+    def close(self):
+        """closes the underlying file if open"""
         if self._fd:
             self._fd.close()
             self._fd = None
 
     def encrypt_to_file(self, destination):
+        """
+        encrypts the underlying file and writes it to destination
+        using incremental reads
+        """
         with open(destination, "wb") as f:
             while True:
                 data = self.read(BUFFER_SIZE_BYTES)
@@ -68,29 +117,57 @@ class FileEncryptor:
                     break
                 f.write(data)
 
+
 class FileDecryptor:
-    #TODO: detect salt from file
-    def __init__(self, path, key):
-        self.key = key
+    """
+    Decrypts a file encrypted with FileEncryptor
+    Needs to detect all crypto material before the
+    decryption key is reconstructed from password.
+    """
+
+    # pylint: disable=invalid-name
+    def __init__(self, path: str, password: str | None = None):
         self.path = path
         self._fd = None
         self.finalized = False
         self.decryptor = None
-        self._init_crypto()
-        self.max_read_pos = os.path.getsize(path) - IV_SIZE_BYTES - TAG_SIZE_BYTES
+        self.max_read_pos = (
+            os.path.getsize(path) - IV_SIZE_BYTES - TAG_SIZE_BYTES - SALT_SIZE_BYTES
+        )
+        if self.max_read_pos < 0:
+            raise IOError(
+                f"File size {os.path.getsize(path)} of {path} is too small to be decrypted."
+                "Could not get all crypto metadata."
+            )
+        self._init_crypto(password=password)
 
-    def _init_crypto(self):
+    def _init_crypto(self, password: str | None = None):
+        """Inits crypto material based on the content of the file and caches the key"""
+        # TODO: ask for password interactively if not provided
+        if password is None:
+            raise NotImplementedError("Password must be provided")
         self._fd = open(self.path, "rb")
-        self._fd.seek(-IV_SIZE_BYTES - TAG_SIZE_BYTES, os.SEEK_END)
+        self._fd.seek(-SALT_SIZE_BYTES - IV_SIZE_BYTES - TAG_SIZE_BYTES, os.SEEK_END)
         self.tag = self._fd.read(TAG_SIZE_BYTES)
         self.iv = self._fd.read(IV_SIZE_BYTES)
+        salt = self._fd.read(SALT_SIZE_BYTES)
         self._fd.seek(0)
+        if _KEYSTORE.get(salt):
+            self.key = _KEYSTORE[salt]
+        else:
+            self.key = EncryptionKey(password=password, salt=salt)
+            _KEYSTORE[salt] = self.key
+
         self.decryptor = Cipher(
-            algorithms.AES256(self.key),
+            algorithms.AES256(self.key.key),
             modes.GCM(self.iv, self.tag),
         ).decryptor()
 
     def read(self, size=None):
+        """
+        reads an encrypted underlying file and
+        decrypts the file content, returns decrypted data
+        """
         if self.finalized:
             return b""
         args = []
@@ -99,78 +176,34 @@ class FileDecryptor:
         else:
             args.append(min(size, self.max_read_pos - self._fd.tell()))
         encrypted_data = self._fd.read(*args)
+        decrypted_data = self.decryptor.update(encrypted_data)
         if not encrypted_data:
             self.finalized = True
             return self.decryptor.finalize()
+        # if we read it all in one go, we need to finalize as there won't be
+        # any other read call coming
+        if self._fd.tell() == self.max_read_pos:
+            self.finalized = True
+            decrypted_data += self.decryptor.finalize()
+        return decrypted_data
 
-        return self.decryptor.update(encrypted_data)
-
-
-def generate_key(password, salt=None):
-    if salt is None:
-        salt = os.urandom(SALT_SIZE_BYTES)
-    kdf = Scrypt(
-        salt=salt,
-        length=32,
-        n=SCRYPT_N,
-        r=8,
-        p=1,
-    )
-    key = kdf.derive(password.encode("utf-8"))
-
-    # verify
-    kdf = Scrypt(
-        salt=salt,
-        length=32,
-        n=SCRYPT_N,
-        r=8,
-        p=1,
-    )
-    kdf.verify(password.encode("utf-8"), key)
-
-    return (key, salt)
+    def close(self):
+        """closes the underlying file if open"""
+        if self._fd:
+            self._fd.close()
+            self._fd = None
 
 
-def get_encryptor(key):
-    """Returns a tuple of (iv, AES256 encryptor)"""
+def _encrypt(key: EncryptionKey, plaintext: bytes):
+    """Encrypts plaintext byte data with the given key and returns a tuple of iv, ciphertext, tag"""
+    # pylint: disable=invalid-name
     # Generate a random 96-bit IV.
     iv = os.urandom(IV_SIZE_BYTES)
 
     # Construct an AES-GCM Cipher object with the given key and a
     # randomly generated IV.
     encryptor = Cipher(
-        algorithms.AES256(key),
-        modes.GCM(iv),
-    ).encryptor()
-
-    return (iv, encryptor)
-
-
-def get_decryptor(key, iv, tag):
-    """
-    Returns an AES256 decryptor
-    key: 256bit key (32 bytes)
-    iv: 96bit iv (12 bytes)
-    tag: 128bit tag (16 bytes)
-    """
-    # Construct a Cipher object, with the key, iv, and additionally the
-    # GCM tag used for authenticating the message.
-    decryptor = Cipher(
-        algorithms.AES256(key),
-        modes.GCM(iv, tag),
-    ).decryptor()
-
-    return decryptor
-
-
-def _encrypt(key, plaintext):
-    # Generate a random 96-bit IV.
-    iv = os.urandom(IV_SIZE_BYTES)
-
-    # Construct an AES-GCM Cipher object with the given key and a
-    # randomly generated IV.
-    encryptor = Cipher(
-        algorithms.AES256(key),
+        algorithms.AES256(key.key),
         modes.GCM(iv),
     ).encryptor()
 
@@ -181,11 +214,13 @@ def _encrypt(key, plaintext):
     return (iv, ciphertext, encryptor.tag)
 
 
-def _decrypt(key, iv, ciphertext, tag):
+def _decrypt(key: EncryptionKey, iv: bytes, ciphertext: bytes, tag: bytes):
+    """Decrypts ciphertext byte data with the given key and returns the plaintext"""
     # Construct a Cipher object, with the key, iv, and additionally the
     # GCM tag used for authenticating the message.
+    # pylint: disable=invalid-name
     decryptor = Cipher(
-        algorithms.AES256(key),
+        algorithms.AES256(key.key),
         modes.GCM(iv, tag),
     ).decryptor()
 
@@ -194,123 +229,55 @@ def _decrypt(key, iv, ciphertext, tag):
     return decryptor.update(ciphertext) + decryptor.finalize()
 
 
-#
-print("Generating key")
-key, salt = generate_key("password")
-#iv, encryptor = get_encryptor(key)
-input_file = "./data"
-encrypted_file = "./data.enc"
-
-
-def encrypt_filename(key, plaintext):
+def encrypt_filename(key: EncryptionKey, plaintext: str) -> bytes:
+    """Encrypts a filename with the given key and returns a base64 encoded string"""
+    # pylint: disable=invalid-name
     iv, ciphertext, tag = _encrypt(key, plaintext.encode("utf-8"))
-    result = base64.urlsafe_b64encode(iv + tag + salt +ciphertext)
-    #log.debug(f"Encryped: {ciphertext} iv={iv} tag={tag}")
+    result = base64.urlsafe_b64encode(iv + tag + key.salt + ciphertext)
     return result
 
 
-def decrypt_filename(key, encrypted_filename):
+def decrypt_filename(encrypted_filename: bytes, password=None) -> str:
+    """Decrypts a filename with the given key and returns a base64 encoded string"""
+    # pylint: disable=invalid-name
+    if password is None:
+        raise NotImplementedError("Password must be provided")
     decoded = base64.urlsafe_b64decode(encrypted_filename)
+    if len(decoded) < IV_SIZE_BYTES + TAG_SIZE_BYTES + SALT_SIZE_BYTES:
+        raise ValueError("Invalid encrypted filename. Too short to get all required metadata.")
     iv = decoded[:IV_SIZE_BYTES]
     tag = decoded[IV_SIZE_BYTES : IV_SIZE_BYTES + TAG_SIZE_BYTES]
-    salt = decoded[IV_SIZE_BYTES + TAG_SIZE_BYTES : IV_SIZE_BYTES + TAG_SIZE_BYTES + SALT_SIZE_BYTES]
+    salt = decoded[
+        IV_SIZE_BYTES
+        + TAG_SIZE_BYTES : IV_SIZE_BYTES
+        + TAG_SIZE_BYTES
+        + SALT_SIZE_BYTES
+    ]
     try:
         key = _KEYSTORE[salt]
     except KeyError:
         log.debug(f"Generating decryption key for salt {salt}")
-        key, _ = generate_key("password", salt)
+        # TODO: ask for password interactively if not provided
+        key = EncryptionKey(password=password, salt=salt)
         _KEYSTORE[salt] = key
     ciphertext = decoded[IV_SIZE_BYTES + TAG_SIZE_BYTES + SALT_SIZE_BYTES :]
-    #log.debug(f"Decryped: {ciphertext} iv={iv} tag={tag}")
     return _decrypt(key, iv, ciphertext, tag).decode("utf-8")
 
 
-def decrypt_file(encrypted_file, destination):
-    file_size = os.path.getsize(encrypted_file)
-    last_read_byte_pos = file_size - TAG_SIZE_BYTES - IV_SIZE_BYTES
-    if last_read_byte_pos < 0:
-        raise IOError(
-            f"File size {file_size} of {encrypted_file} is too small to be decrypted (does not contain TAG and IV at least)."
-        )
-    with open(encrypted_file, "rb") as ef:
-        # seek to tag+iv entry
-        ef.seek(file_size - TAG_SIZE_BYTES - IV_SIZE_BYTES)
-        tag = ef.read(TAG_SIZE_BYTES)
-        iv = ef.read(IV_SIZE_BYTES)
-        ef.seek(0)
-        decryptor = get_decryptor(key, iv, tag)
-
-        with open(destination, "wb") as f:
-            while True:
-                if ef.tell() + BUFFER_SIZE_BYTES <= last_read_byte_pos:
-                    data = ef.read(BUFFER_SIZE_BYTES)
-                    f.write(decryptor.update(data))
-                else:
-                    data = ef.read(last_read_byte_pos - ef.tell())
-                    f.write(decryptor.update(data))
-                    f.write(decryptor.finalize())
-                    break
-
-def test():
-    encrypted_filename = encrypt_filename(key, input_file * 8)
-    print("Encrypted filename: %s" % encrypted_filename)
-    print("Decrypted filename: %s" % decrypt_filename(key, encrypted_filename))
-    print("Encrypting file")
-    file_encryptor = FileEncryptor(input_file, key)
-    with open(encrypted_file, "wb") as ef:
-        data = file_encryptor.read()
-        ef.write(data)
-
-    print("Decrypting file")
-    file_decryptor = FileDecryptor(encrypted_file, key)
-    with open("./data.dec", "wb") as f:
-        data = file_decryptor.read()
-        f.write(data)
-
-def encrypt(directory, destination):
+def encrypt_directory(key: EncryptionKey, directory, destination):
+    """encrypts all files and directories in directory and writes them to destination"""
     # todo: make sure destination is empty
+
     for root, dirs, files in os.walk(directory):
-        for f in files:
-            encrypted_filename = encrypt_filename(key, f).decode("utf-8")
-            log.debug(f"Encrypting file {f} -> {encrypted_filename}")
-            file_encryptor = FileEncryptor(os.path.join(root, f), key)
-            file_encryptor.encrypt_to_file(os.path.join(destination, encrypted_filename))
-            
+        for fname in files:
+            encrypted_filename = encrypt_filename(key, fname).decode("utf-8")
+            log.debug(f"Encrypting file {fname} -> {encrypted_filename}")
+            file_encryptor = FileEncryptor(os.path.join(root, fname), key)
+            file_encryptor.encrypt_to_file(
+                os.path.join(destination, encrypted_filename)
+            )
+
         # for d in dirs:
         #     encrypted_dir = encrypt_filename(key, d).decode("utf-8")
         #     log.debug(f"Encrypting directory {d} -> {encrypted_dir}")
         #     os.mkdir(os.path.join(destination, encrypted_dir))
-
-def list_encrypted_dir(encrypted_dir):
-    for root, dirs, files in os.walk(encrypted_dir):
-        for f in files:
-            try:
-                print(f"File: {decrypt_filename(key, f)} - {f}")
-            except Exception as e:
-                print(f"Error while decrypting {f}: {e.__class__} {e}")
-                
-        for d in dirs:
-            print(f"Directory: {d}")            
-            
-
-
-EDIR = "./plain"
-CDIR = "./encrypted"
-encrypt(EDIR, CDIR)
-list_encrypted_dir(CDIR)
-
-x = encrypt_filename(key, "test")
-print(x)
-#with open('/tmp/test', 'wb') as f:
-#    f.write(x)
-
-with open('/tmp/test', 'rb') as f:
-    y = f.read()
-   
-try:
-    print(decrypt_filename(key, y))
-except Exception as e:
-    print(f"Error while decrypting {y}: {e.__class__} {e}")
-
-# DEBUG:__main__:Decryped: b'8\xeb\xbe\x83' iv=b'\r\x865x*\xe6T\xbf\xad\x9a`\xc5' tag=b'\xe98:\xf4no\xe0\x06/L\xb2z\xe3\xcc\x82\xa9'
-# DEBUG:__main__:Decryped: b'8\xeb\xbe\x83' iv=b'\r\x865x*\xe6T\xbf\xad\x9a`\xc5' tag=b'\xe98:\xf4no\xe0\x06/L\xb2z\xe3\xcc\x82\xa9'
---
+++ tests.py
@@ -0,0 +1,187 @@
+import os
+import backup
+import unittest
+from backup import EncryptionKey
+import tempfile
+import cryptography.exceptions
+import base64
+
+
+class EncryptionKeyTest(unittest.TestCase):
+    def test_new_key(self):
+        password = "test"
+        key = EncryptionKey(password=password)
+        self.assertEqual(len(key.key), 32)
+        self.assertEqual(len(key.salt), backup.SALT_SIZE_BYTES)
+
+    def test_key_from_salt(self):
+        password = "test"
+        key = EncryptionKey(password=password)
+        key2 = EncryptionKey(password=password, salt=key.salt)
+        self.assertEqual(key.key, key2.key)
+        self.assertEqual(key.salt, key2.salt)
+
+
+class FileEncryptorTest(unittest.TestCase):
+    def setUp(self):
+        self.created_files = []
+        self.test_dir = tempfile.mkdtemp(prefix="backup_test_")
+        self.test_file = self.get_temp_file()
+        self.test_data = b"test" * 1024 * 1024
+        with open(self.test_file, "wb") as tfd:
+            tfd.write(self.test_data)
+        self.password = "test"
+
+    def add_file(self, path):
+        self.created_files.append(path)
+
+    def get_temp_file(self):
+        fd, path = tempfile.mkstemp(dir=self.test_dir)
+        os.close(fd)
+        self.add_file(path)
+        return path
+
+    def tearDown(self):
+        for f in self.created_files:
+            os.remove(os.path.join(self.test_dir, f))
+        os.rmdir(self.test_dir)
+
+    def test_encrypt_file(self):
+        key = EncryptionKey(password=self.password)
+        # encrypt buffered
+        encryptor = backup.FileEncryptor(path=self.test_file, key=key)
+        encrypted_data = b""
+        buffer_size = 1024
+        while True:
+            new_data = encryptor.read(buffer_size)
+            if not new_data:
+                break
+            encrypted_data += new_data
+        encryptor.close()
+        # encrypt all at once with new instance
+        encryptor = backup.FileEncryptor(path=self.test_file, key=key)
+        encrypted_data_unbuf = encryptor.read()
+        encryptor.close()
+        # compare the size of the encrypted data, content will differ
+        # due to the random IV
+        self.assertEqual(len(encrypted_data), len(encrypted_data_unbuf))
+
+    def test_decrypt_file(self):
+        key = EncryptionKey(password=self.password)
+        enc_buf = backup.FileEncryptor(path=self.test_file, key=key)
+        enc_buf_file = self.get_temp_file()
+        buffer_size = 1024
+        with open(enc_buf_file, "wb") as tfd:
+            while True:
+                data = enc_buf.read(buffer_size)
+                if not data:
+                    break
+                tfd.write(data)
+        enc_buf.close()
+        enc_unbuf = backup.FileEncryptor(path=self.test_file, key=key)
+        enc_unbuf_file = self.get_temp_file()
+        with open(enc_unbuf_file, "wb") as tfd:
+            while True:
+                data = enc_unbuf.read()
+                if not data:
+                    break
+                tfd.write(data)
+        enc_unbuf.close()
+
+        # decrypt
+        dec_buf = backup.FileDecryptor(path=enc_buf_file, password=self.password)
+        dec_buf_data = b""
+        while True:
+            new_data = dec_buf.read(buffer_size)
+            if not new_data:
+                break
+            dec_buf_data += new_data
+        dec_buf.close()
+        dec_unbuf = backup.FileDecryptor(path=enc_unbuf_file, password=self.password)
+        dec_unbuf_data = dec_unbuf.read()
+        dec_unbuf.close()
+        self.assertEqual(dec_buf_data, dec_unbuf_data)
+        self.assertEqual(dec_buf_data, self.test_data)
+
+    def test_encrypt_to_file(self):
+        key = EncryptionKey(password=self.password)
+        enc_file = self.get_temp_file()
+        enc = backup.FileEncryptor(path=self.test_file, key=key)
+        enc.encrypt_to_file(enc_file)
+        enc.close()
+        # decrypt
+        dec = backup.FileDecryptor(path=enc_file, password=self.password)
+        dec_data = dec.read()
+        dec.close()
+        self.assertEqual(dec_data, self.test_data)
+
+    def test_invalid_data_decryption(self):
+        key = EncryptionKey(password=self.password)
+        enc_file = self.get_temp_file()
+        enc = backup.FileEncryptor(path=self.test_file, key=key)
+        enc.encrypt_to_file(enc_file)
+        enc.close()
+        # corrupt data
+        with open(enc_file, "r+b") as tfd:
+            tfd.seek(0)
+            tfd.write(b"invalid")
+        # decrypt buffered
+        dec = backup.FileDecryptor(path=enc_file, password=self.password)
+        dec_data = b""
+        buffer_size = 1024
+        with self.assertRaises(cryptography.exceptions.InvalidTag):
+            while True:
+                new_data = dec.read(buffer_size)
+                if not new_data:
+                    break
+                dec_data += new_data
+        dec.close()
+        # decrypt unbuffered
+        dec = backup.FileDecryptor(path=enc_file, password=self.password)
+        with self.assertRaises(cryptography.exceptions.InvalidTag):
+            dec_data = dec.read()
+        dec.close()
+
+    def test_plaintext_not_in_encrypted_data(self):
+        key = EncryptionKey(password=self.password)
+        encryptor = backup.FileEncryptor(path=self.test_file, key=key)
+        encrypted_data = encryptor.read()
+        encryptor.close()
+        self.assertNotEqual(encrypted_data, self.test_data)
+        self.assertNotIn(self.test_data[:20], encrypted_data)
+
+    def test_decrypt_empty_file(self):
+        key = EncryptionKey(password=self.password)
+        enc_file = self.get_temp_file()
+        with self.assertRaises(IOError):
+            backup.FileDecryptor(path=enc_file, password=self.password)
+        
+
+        
+
+class FileNameEncryptionTest(unittest.TestCase):
+    def test_encrypt_filename(self):
+        password = "test"
+        key = EncryptionKey(password=password)
+        filename = "test.txt"
+        encrypted_name = backup.encrypt_filename(key, filename)
+        self.assertNotEqual(filename, encrypted_name)
+        self.assertEqual(filename, backup.decrypt_filename(encrypted_name, password=password))
+
+    def test_decrypt_corrupted_filename(self):
+        password = "test"
+        key = EncryptionKey(password=password)
+        filename = "test.txt"
+        encrypted_name = backup.encrypt_filename(key, filename)
+        encrypted_name = b"INVALID"+encrypted_name[7:]
+        with self.assertRaises(cryptography.exceptions.InvalidTag):
+            backup.decrypt_filename(encrypted_name, password=password)
+            
+    def test_decrypt_too_short(self):
+        filename=b"test"
+        invalid_crypto_data = base64.urlsafe_b64encode(filename)
+        with self.assertRaises(ValueError):
+            backup.decrypt_filename(invalid_crypto_data, password="test")
+
+if __name__ == "__main__":
+    unittest.main()
