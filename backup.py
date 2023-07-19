808c647c702575a81675352b5b0d5c73253618ac
jk <jk@spasitel.com>
2023-07-19 18:13:36 +0200

    fix of the command line tool to recent changes

--- backup.py
+++ backup.py
@@ -5,6 +5,8 @@ import argparse
 import logging
 import sys
 import base
+from base import FSDirectory
+from filesystems import RealFilesystem
 
 logging.basicConfig()
 
@@ -14,11 +16,22 @@ log.setLevel(logging.WARNING)
 # pylint: disable=broad-except
 
 
+def set_password(password):
+    """
+    helper function to set password in base module
+    and not duplicate code in main function
+    """
+    if not password:
+        password = base.get_password()
+    # TODO: private member should not be accessed directly
+    base._PASSWORD = password
+
+
 def main():
     """Main function"""
     # parse cmdline args
     parser = argparse.ArgumentParser(
-        description="Encrypt source directory to target directory."
+        description="Encrypt / decrypt source directory to target directory."
     )
     parser.add_argument(
         "--encrypt",
@@ -49,15 +62,24 @@ def main():
     if args.debug:
         base.log.setLevel(logging.DEBUG)
 
-    if not args.password:
-        args.password = base.get_password()
-    # TODO: private member should not be accessed directly
-    base._PASSWORD = args.password
     status_reporter = base.StatusReporter()
     base.STATUS_REPORTER = status_reporter
+    # real filesystem hardcoded now temporarily
+    source_filesystem = RealFilesystem()
+    target_filesystem = RealFilesystem()
     if args.encrypt:
+        set_password(args.password)
         try:
-            base.encrypt_directory(args.encrypt[0], args.encrypt[1])
+            if not target_filesystem.exists(args.encrypt[1]):
+                target_filesystem.makedirs(args.encrypt[1])
+            base.encrypt_directory(
+                FSDirectory.from_filesystem(
+                    args.encrypt[0], filesystem=source_filesystem
+                ),
+                FSDirectory.from_filesystem(
+                    args.encrypt[1], filesystem=target_filesystem
+                ),
+            )
             print(
                 f"\nEncryption successfully finished. "
                 f"Encrypted files: {status_reporter.files_processed}"
@@ -65,11 +87,21 @@ def main():
             )
         except Exception as ex:
             log.debug(f"Exception: {ex}", exc_info=True)
-            print("Error: " + str(ex), file=sys.stderr)
+            log.error("Error: " + str(ex))
             sys.exit(2)
     elif args.decrypt:
+        set_password(args.password)
+        if not target_filesystem.exists(args.decrypt[1]):
+            target_filesystem.makedirs(args.decrypt[1])
         try:
-            base.decrypt_directory(args.decrypt[0], args.decrypt[1])
+            base.decrypt_directory(
+                FSDirectory.from_filesystem(
+                    args.decrypt[0], filesystem=source_filesystem
+                ),
+                FSDirectory.from_filesystem(
+                    args.decrypt[1], filesystem=target_filesystem
+                ),
+            )
             print(
                 f"\nEncryption successfully finished. "
                 f"Decrypted files: {status_reporter.files_processed}"
@@ -80,8 +112,11 @@ def main():
             print("Error: " + str(ex), file=sys.stderr)
             sys.exit(2)
     elif args.list:
+        set_password(args.password)
         try:
-            directory = base.FSDirectory.from_filesystem(args.list[0])
+            directory = FSDirectory.from_filesystem(
+                args.list[0], filesystem=source_filesystem
+            )
             directory.pretty_print()
 
         except Exception as ex:
@@ -90,7 +125,8 @@ def main():
             sys.exit(2)
 
     else:
-        log.error("Invalid operation: %s", args.operation)
+        log.error("No action specified")
+        parser.print_help()
         sys.exit(1)
 
 
